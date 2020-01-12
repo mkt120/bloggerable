77e7f873cdb3763567b693b7618ab89df5fff4c7
どっこと <dev.mkt120.elop@gmail.com>
2020-01-12 22:53:33 +0900

    markdown to HTML

--- app/src/main/java/com/mkt120/bloggerable/CreatePostsActivity.kt
+++ app/src/main/java/com/mkt120/bloggerable/CreatePostsActivity.kt
@@ -3,14 +3,16 @@ package com.mkt120.bloggerable
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.Html
-import android.text.SpannableStringBuilder
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension
+import com.vladsch.flexmark.ext.tables.TablesExtension
+import com.vladsch.flexmark.html.HtmlRenderer
+import com.vladsch.flexmark.parser.Parser
+import com.vladsch.flexmark.util.data.MutableDataSet
 import kotlinx.android.synthetic.main.activity_create_post.*
 
 class CreatePostsActivity : AppCompatActivity() {
-
     companion object {
         private const val EXTRA_KEY_BLOG_ID = "EXTRA_KEY_BLOG_ID"
 
@@ -32,7 +34,7 @@ class CreatePostsActivity : AppCompatActivity() {
     private fun createPosts() {
         val blogId = intent.getStringExtra(EXTRA_KEY_BLOG_ID)!!
         val title = edit_text_title.text.toString()
-        val contentTemporary = edit_text_contents.text.toString()
+        val content = edit_text_contents.text.toString()
 
         if (title.isEmpty()) {
             // 空 empty title
@@ -40,14 +42,28 @@ class CreatePostsActivity : AppCompatActivity() {
             return
         }
 
-        val span = SpannableStringBuilder(contentTemporary)
-        val content = Html.toHtml(span, 0)
+        // 改行文字をHTMLに差し替え
+        val options = MutableDataSet()
+        options.set(
+            Parser.EXTENSIONS,
+            listOf(TablesExtension.create(), StrikethroughExtension.create())
+        )
 
-        ApiManager.createPosts(blogId, title, content, object : ApiManager.CompleteListener {
+        // Markdown to HTML
+        val parser = Parser.builder(options).build()
+        val renderer = HtmlRenderer.builder(options).build()
+        val document = parser.parse(content)
+        val html = renderer.render(document)
+
+        // create Markdown string
+        // val markdown = FlexmarkHtmlConverter.builder(options).build().convert(html)
+
+        ApiManager.createPosts(blogId, title, html, object : ApiManager.CompleteListener {
             override fun onComplete() {
                 Toast.makeText(this@CreatePostsActivity, "成功しました", Toast.LENGTH_SHORT).show()
                 finish()
             }
+
             override fun onFailed(t: Throwable) {
                 Toast.makeText(this@CreatePostsActivity, "失敗しました", Toast.LENGTH_SHORT).show()
             }
