81eb6236bc643875da37b4d17e7ada82bfdf4bcd
どっこと <dev.mkt120.elop@gmail.com>
2020-01-13 17:29:55 +0900

    新規投稿画面のレイアウトを更新、string、dimensを更新

--- app/src/main/java/com/mkt120/bloggerable/CreatePostsActivity.kt
+++ app/src/main/java/com/mkt120/bloggerable/CreatePostsActivity.kt
@@ -1,10 +1,13 @@
 package com.mkt120.bloggerable
 
+import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension
 import com.vladsch.flexmark.ext.tables.TablesExtension
 import com.vladsch.flexmark.html.HtmlRenderer
@@ -12,7 +15,11 @@ import com.vladsch.flexmark.parser.Parser
 import com.vladsch.flexmark.util.data.MutableDataSet
 import kotlinx.android.synthetic.main.activity_create_post.*
 
-class CreatePostsActivity : AppCompatActivity() {
+/**
+ * 新規投稿画面 CreatePostsScreen
+ */
+class CreatePostsActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
+
     companion object {
         private const val EXTRA_KEY_BLOG_ID = "EXTRA_KEY_BLOG_ID"
 
@@ -26,11 +33,84 @@ class CreatePostsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
 
-        tool_bar.title = "新規投稿"
+        tool_bar.setTitle(R.string.create_posts_title)
+        tool_bar.inflateMenu(R.menu.create_posts_menu)
+        tool_bar.setOnMenuItemClickListener(this)
 
-        button_create.setOnClickListener {
-            createPosts()
+        button_add_bold.setOnClickListener {
+            addBold()
         }
+
+        button_add_italic.setOnClickListener {
+            addItalic()
+        }
+
+        button_add_strike_through.setOnClickListener {
+            addStrikeThrough()
+        }
+
+        button_add_paste.setOnClickListener {
+            addPaste()
+        }
+    }
+
+    private fun addBold() {
+        val text = edit_text_contents.text
+        val start = edit_text_contents.selectionStart
+        val end = edit_text_contents.selectionEnd
+        val middle: String = text.substring(start, end)
+        add("**$middle**", start + middle.length + 2)
+    }
+
+    private fun addItalic() {
+        val text = edit_text_contents.text
+        val start = edit_text_contents.selectionStart
+        val end = edit_text_contents.selectionEnd
+        val middle: String = text.substring(start, end)
+        add("*$middle*", start + middle.length + 1)
+    }
+
+    private fun addStrikeThrough() {
+        val text = edit_text_contents.text
+        val start = edit_text_contents.selectionStart
+        val end = edit_text_contents.selectionEnd
+        val middle: String = text.substring(start, end)
+        add("~~$middle~~", start + middle.length + 2)
+    }
+
+    private fun addPaste() {
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        if (clipboard.primaryClip == null) {
+            return
+        }
+        val item = clipboard.primaryClip!!.getItemAt(0)
+        val start = edit_text_contents.selectionStart
+        add(item.text.toString(), start + item.text.length)
+    }
+
+    private fun add(
+        middle: String,
+        selection: Int
+    ) {
+        val start = edit_text_contents.selectionStart
+        val end = edit_text_contents.selectionEnd
+
+        val text = edit_text_contents.text
+        val former = text.substring(0, start)
+        val latter = text.substring(end)
+
+        edit_text_contents.setText("$former$middle$latter")
+        edit_text_contents.setSelection(selection)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when {
+            item?.itemId == R.id.create_posts -> {
+                createPosts()
+                return true
+            }
+        }
+        return false
     }
 
     private fun createPosts() {
@@ -40,18 +120,16 @@ class CreatePostsActivity : AppCompatActivity() {
 
         if (title.isEmpty()) {
             // 空 empty title
-            Toast.makeText(this@CreatePostsActivity, "タイトルがありません", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@CreatePostsActivity, R.string.toast_error_create_posts_no_title, Toast.LENGTH_SHORT).show()
             return
         }
 
-        // 改行文字をHTMLに差し替え
+        // Markdown to HTML
         val options = MutableDataSet()
         options.set(
             Parser.EXTENSIONS,
             listOf(TablesExtension.create(), StrikethroughExtension.create())
         )
-
-        // Markdown to HTML
         val parser = Parser.builder(options).build()
         val renderer = HtmlRenderer.builder(options).build()
         val document = parser.parse(content)
@@ -62,12 +140,12 @@ class CreatePostsActivity : AppCompatActivity() {
 
         ApiManager.createPosts(blogId, title, html, object : ApiManager.CompleteListener {
             override fun onComplete() {
-                Toast.makeText(this@CreatePostsActivity, "成功しました", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@CreatePostsActivity, R.string.toast_create_posts_success, Toast.LENGTH_SHORT).show()
                 finish()
             }
 
             override fun onFailed(t: Throwable) {
-                Toast.makeText(this@CreatePostsActivity, "失敗しました", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@CreatePostsActivity, R.string.toast_create_posts_failed, Toast.LENGTH_SHORT).show()
             }
         })
     }
