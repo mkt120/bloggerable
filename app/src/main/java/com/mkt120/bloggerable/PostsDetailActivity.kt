687a5231f6ea14123dba951fbf8dfcf6a418782a
どっこと <dev.mkt120.elop@gmail.com>
2020-01-14 23:03:48 +0900

    詳細画面を更新

--- app/src/main/java/com/mkt120/bloggerable/PostsDetailActivity.kt
+++ app/src/main/java/com/mkt120/bloggerable/PostsDetailActivity.kt
@@ -2,15 +2,18 @@ package com.mkt120.bloggerable
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Base64
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.mkt120.bloggerable.model.Posts
 import kotlinx.android.synthetic.main.activity_posts_detail.*
 
-class PostsDetailActivity : AppCompatActivity() {
+class PostsDetailActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
 
     companion object {
         private const val EXTRA_KEY_BLOG_ID = "EXTRA_KEY_BLOG_ID"
@@ -33,8 +36,9 @@ class PostsDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts_detail)
 
-        tool_bar.title = "投稿詳細"
-
+        tool_bar.setTitle(R.string.posts_detail_title)
+        tool_bar.inflateMenu(R.menu.posts_detail_menu)
+        tool_bar.setOnMenuItemClickListener(this)
 
         val posts: Posts = intent.getParcelableExtra(EXTRA_KEY_POSTS)!!
         title_view.text = posts.title
@@ -46,13 +50,28 @@ class PostsDetailActivity : AppCompatActivity() {
             Base64.encode(content.toByteArray(), Base64.DEFAULT).toString()
         }
         contents_view.loadData(b64Encode, "text/html", "base64")
+    }
 
-        button_delete.setOnClickListener {
-            // 削除ボタン
-            val postsId: String = intent.getStringExtra(EXTRA_KEY_POSTS_ID)!!
-            val blogId: String = intent.getStringExtra(EXTRA_KEY_BLOG_ID)!!
-            deletePosts(blogId, postsId)
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.open_in_browser -> {
+                val posts: Posts = intent.getParcelableExtra(EXTRA_KEY_POSTS)!!
+                val i = Intent(Intent.ACTION_VIEW, Uri.parse(posts.url))
+                startActivity(i)
+                return true
+            }
+            R.id.delete_posts -> {
+                // 削除ボタン
+                val postsId: String = intent.getStringExtra(EXTRA_KEY_POSTS_ID)!!
+                val blogId: String = intent.getStringExtra(EXTRA_KEY_BLOG_ID)!!
+                deletePosts(blogId, postsId)
+                return true
+            }
+            R.id.edit_posts -> {
+                return false
+            }
         }
+        return false
     }
 
     private fun deletePosts(blogId: String, postsId: String) {
@@ -61,15 +80,12 @@ class PostsDetailActivity : AppCompatActivity() {
             postsId,
             object : ApiManager.CompleteListener {
                 override fun onComplete() {
-                    Toast.makeText(this@PostsDetailActivity, "削除できました", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@PostsDetailActivity, R.string.toast_detail_posts_success_delete, Toast.LENGTH_SHORT).show()
                     finish()
                 }
-
                 override fun onFailed(t: Throwable) {
-                    Toast.makeText(this@PostsDetailActivity, "削除できませんでした", Toast.LENGTH_SHORT)
-                        .show()
+                    Toast.makeText(this@PostsDetailActivity, R.string.toast_detail_posts_failed_delete, Toast.LENGTH_SHORT).show()
                 }
-
             })
     }
 }
