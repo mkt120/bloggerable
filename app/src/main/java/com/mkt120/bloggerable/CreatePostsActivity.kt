122048d84e2bc67b042f4fb555887f6106cebebe
どっこと <dev.mkt120.elop@gmail.com>
2020-02-01 23:36:00 +0900

    【新規投稿】フォントサイズを変更する処理を追加,textStyleをSpannableStringBuilderを使って変えるように変更

--- app/src/main/java/com/mkt120/bloggerable/CreatePostsActivity.kt
+++ app/src/main/java/com/mkt120/bloggerable/CreatePostsActivity.kt
@@ -4,20 +4,21 @@ import android.app.Activity
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Html
+import android.text.ParcelableSpan
+import android.text.Spannable
+import android.text.style.RelativeSizeSpan
+import android.text.style.StrikethroughSpan
+import android.text.style.StyleSpan
+import android.util.Log
 import android.view.MenuItem
-import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.DialogFragment
-import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension
-import com.vladsch.flexmark.ext.tables.TablesExtension
-import com.vladsch.flexmark.html.HtmlRenderer
-import com.vladsch.flexmark.parser.Parser
-import com.vladsch.flexmark.util.data.MutableDataSet
 import kotlinx.android.synthetic.main.activity_create_post.*
 
 /**
@@ -28,6 +29,9 @@ class CreatePostsActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener
 
     companion object {
         private const val EXTRA_KEY_BLOG_ID = "EXTRA_KEY_BLOG_ID"
+        private const val RELATIVE_FONT_SIZE_X_LARGE = 2.0f
+        private const val RELATIVE_FONT_SIZE_LARGE = 1.5f
+        private val TAG = CreatePostsActivity::class.java.simpleName
 
         fun createIntent(context: Context, blogId: String): Intent =
             Intent(context, CreatePostsActivity::class.java).apply {
@@ -79,6 +83,9 @@ class CreatePostsActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener
             dialogFragment = AddLabelHistoryDialogFragment.newInstance()
             dialogFragment!!.show(supportFragmentManager, null)
         }
+        button_add_font_change.setOnClickListener {
+            addRelativeSize()
+        }
     }
 
     override fun addLabel(label: String) {
@@ -101,53 +108,217 @@ class CreatePostsActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener
         }
     }
 
+    /**
+     * boldを追加・削除する
+     */
     private fun addBold() {
-        val text = edit_text_contents.text
-        val start = edit_text_contents.selectionStart
-        val end = edit_text_contents.selectionEnd
-        val middle: String = text.substring(start, end)
-        add("**$middle**", start + middle.length + 2)
+        Log.i(TAG, "addBold")
+        val cursorStart = Math.min(edit_text_contents.selectionStart, edit_text_contents.selectionEnd)
+        val cursorEnd = Math.max(edit_text_contents.selectionStart, edit_text_contents.selectionEnd)
+        val spans = edit_text_contents.text.getSpans(cursorStart, cursorEnd, StyleSpan::class.java)
+        if (spans.isEmpty()) {
+            // boldをつけるだけ
+            addSpan(StyleSpan(Typeface.BOLD))
+            edit_text_contents.setSelection(cursorStart, cursorEnd)
+            return
+        }
+        val hasBold = spans.any  { (it.style != Typeface.ITALIC) }
+        if (!hasBold) {
+            // boldをつけるだけ
+            addSpan(StyleSpan(Typeface.BOLD))
+            edit_text_contents.setSelection(cursorStart, cursorEnd)
+            return
+        }
+
+        // boldを外すだけ
+        for (span in spans) {
+            if (span.style == Typeface.ITALIC) {
+                continue
+            }
+            val spanLeft = Math.min(edit_text_contents.text.getSpanStart(span), edit_text_contents.text.getSpanEnd(span))
+            val spanRight = Math.max(edit_text_contents.text.getSpanStart(span), edit_text_contents.text.getSpanEnd(span))
+            edit_text_contents.text.removeSpan(span)
+            if (spanLeft < cursorStart || cursorEnd < spanRight) {
+                // 選択範囲が1つのspanに含まれている
+                var clone = StyleSpan(span.style)
+                if (spanLeft < cursorStart) {
+                    // 左が出てる 左にspanつけなおし
+                    addSpan(clone, spanLeft, cursorStart)
+                    clone = StyleSpan(span.style)
+                }
+                if (cursorEnd < spanRight) {
+                    // 右が出てる 右にspanつけなおし
+                    addSpan(clone, cursorEnd, spanRight)
+                }
+            }
+        }
+        edit_text_contents.setSelection(cursorStart, cursorEnd)
     }
 
+    /**
+     * Italicを追加・削除する
+     */
     private fun addItalic() {
-        val text = edit_text_contents.text
-        val start = edit_text_contents.selectionStart
-        val end = edit_text_contents.selectionEnd
-        val middle: String = text.substring(start, end)
-        add("*$middle*", start + middle.length + 1)
+        Log.i(TAG, "addItalic")
+        val cursorStart = Math.min(edit_text_contents.selectionStart, edit_text_contents.selectionEnd)
+        val cursorEnd = Math.max(edit_text_contents.selectionStart, edit_text_contents.selectionEnd)
+        val spans = edit_text_contents.text.getSpans(cursorStart, cursorEnd, StyleSpan::class.java)
+        if (spans.isEmpty()) {
+            // italicをつけるだけ
+            addSpan(StyleSpan(Typeface.ITALIC))
+            edit_text_contents.setSelection(cursorStart, cursorEnd)
+            return
+        }
+        val hasBold = spans.any  { (it.style != Typeface.BOLD) }
+        if (!hasBold) {
+            // italicをつけるだけ
+            addSpan(StyleSpan(Typeface.ITALIC))
+            edit_text_contents.setSelection(cursorStart, cursorEnd)
+            return
+        }
+
+        // italic外すだけ
+        for (span in spans) {
+            if (span.style == Typeface.BOLD) {
+                continue
+            }
+            val spanLeft = Math.min(edit_text_contents.text.getSpanStart(span), edit_text_contents.text.getSpanEnd(span))
+            val spanRight = Math.max(edit_text_contents.text.getSpanStart(span), edit_text_contents.text.getSpanEnd(span))
+            edit_text_contents.text.removeSpan(span)
+            if (spanLeft < cursorStart || cursorEnd < spanRight) {
+                // 選択範囲が1つのspanに含まれている
+                if (spanLeft < cursorStart) {
+                    // 左が出てる 左にspanつけなおし
+                    val clone = StyleSpan(span.style)
+                    addSpan(clone, spanLeft, cursorStart)
+                }
+                if (cursorEnd < spanRight) {
+                    // 右が出てる 右にspanつけなおし
+                    val clone = StyleSpan(span.style)
+                    addSpan(clone, cursorEnd, spanRight)
+                }
+            }
+        }
+        edit_text_contents.setSelection(cursorStart, cursorEnd)
     }
 
+    /**
+     * 打消し線を追加・削除する
+     */
     private fun addStrikeThrough() {
-        val text = edit_text_contents.text
-        val start = edit_text_contents.selectionStart
-        val end = edit_text_contents.selectionEnd
-        val middle: String = text.substring(start, end)
-        add("~~$middle~~", start + middle.length + 2)
+        Log.i(TAG, "addStrikeThrough")
+        val cursorStart = Math.min(edit_text_contents.selectionStart, edit_text_contents.selectionEnd)
+        val cursorEnd = Math.max(edit_text_contents.selectionStart, edit_text_contents.selectionEnd)
+        val spans = edit_text_contents.text.getSpans(cursorStart, cursorEnd, StrikethroughSpan::class.java)
+        if (spans.isEmpty()) {
+            addSpan(StrikethroughSpan())
+            edit_text_contents.setSelection(cursorStart, cursorEnd)
+            return
+        }
+        for (span in spans) {
+            val spanLeft = Math.min(edit_text_contents.text.getSpanStart(span), edit_text_contents.text.getSpanEnd(span))
+            val spanRight = Math.max(edit_text_contents.text.getSpanStart(span), edit_text_contents.text.getSpanEnd(span))
+            edit_text_contents.text.removeSpan(span)
+            if (spanLeft < cursorStart || cursorEnd < spanRight) {
+                // 選択範囲が1つのspanに含まれている
+                if (spanLeft < cursorStart) {
+                    // 左が出てる 左にspanつけなおし
+                    val clone = StrikethroughSpan()
+                    addSpan(clone, spanLeft, cursorStart)
+                }
+                if (cursorEnd < spanRight) {
+                    val clone = StrikethroughSpan()
+                    // 右が出てる 右にspanつけなおし
+                    addSpan(clone, cursorEnd, spanRight)
+                }
+            }
+        }
+        edit_text_contents.setSelection(cursorStart, cursorEnd)
     }
 
+    /**
+     * クリップボードにあるテキストを張り付ける
+     */
     private fun addPaste() {
+        Log.i(TAG, "addPaste")
         val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         if (clipboard.primaryClip == null) {
             return
         }
         val item = clipboard.primaryClip!!.getItemAt(0)
-        val start = edit_text_contents.selectionStart
-        add(item.text.toString(), start + item.text.length)
+        val left = Math.min(edit_text_contents.selectionStart, edit_text_contents.selectionEnd)
+        val right = Math.max(edit_text_contents.selectionStart, edit_text_contents.selectionEnd)
+        val clip = item.text.toString()
+        edit_text_contents.text.replace(left, right, clip)
+        edit_text_contents.setSelection(left + clip.length)
     }
 
-    private fun add(
-        middle: String,
-        selection: Int
-    ) {
-        val start = edit_text_contents.selectionStart
-        val end = edit_text_contents.selectionEnd
+    /**
+     * spanを追加する
+     */
+    private fun addSpan(span: ParcelableSpan) {
+        val left = Math.min(edit_text_contents.selectionStart, edit_text_contents.selectionEnd)
+        val right = Math.max(edit_text_contents.selectionStart, edit_text_contents.selectionEnd)
+        addSpan(span, left, right)
+    }
 
-        val text = edit_text_contents.text
-        val former = text.substring(0, start)
-        val latter = text.substring(end)
+    /**
+     * spanを追加する
+     */
+    private fun addSpan(span: ParcelableSpan, left:Int, right:Int) {
+        Log.i("CreatePostsActivity", "addSpan left=$left, right=$right, span=$span")
+        val spannable :Spannable = edit_text_contents.text
+        spannable.setSpan(span, left, right, Spannable.SPAN_EXCLUSIVE_INCLUSIVE)
+        edit_text_contents.setText(spannable)
+    }
 
-        edit_text_contents.setText("$former$middle$latter")
-        edit_text_contents.setSelection(selection)
+    /**
+     * 文字サイズを変更する
+     */
+    private fun addRelativeSize() {
+        Log.i(TAG, "addRelativeSize")
+        val cursorLeft = Math.min(edit_text_contents.selectionStart, edit_text_contents.selectionEnd)
+        val cursorRight = Math.max(edit_text_contents.selectionStart, edit_text_contents.selectionEnd)
+        val spans = edit_text_contents.text.getSpans(cursorLeft, cursorRight, RelativeSizeSpan::class.java)
+        if (spans.isEmpty()) {
+            addSpan(RelativeSizeSpan(RELATIVE_FONT_SIZE_LARGE))
+            edit_text_contents.setSelection(cursorLeft, cursorRight)
+            return
+        }
+        if (spans.size == 1) {
+            val spanLeft = Math.min(edit_text_contents.text.getSpanStart(spans[0]), edit_text_contents.text.getSpanEnd(spans[0]))
+            val spanRight = Math.max(edit_text_contents.text.getSpanStart(spans[0]), edit_text_contents.text.getSpanEnd(spans[0]))
+            if (spanLeft == cursorLeft && spanRight == cursorRight) {
+                // 完全に一致
+                edit_text_contents.text.removeSpan(spans[0])
+                if (spans[0].sizeChange == RELATIVE_FONT_SIZE_LARGE) {
+                    addSpan(RelativeSizeSpan(RELATIVE_FONT_SIZE_X_LARGE), cursorLeft, cursorRight)
+                }
+                edit_text_contents.setSelection(cursorLeft, cursorRight)
+                return
+            }
+        }
+
+        // それ以外は一番大きいサイズに合わせてspanを付与
+        for (span in spans) {
+            val spanLeft = Math.min(edit_text_contents.text.getSpanStart(span), edit_text_contents.text.getSpanEnd(span))
+            val spanRight = Math.max(edit_text_contents.text.getSpanStart(span), edit_text_contents.text.getSpanEnd(span))
+            edit_text_contents.text.removeSpan(span)
+            if (spanLeft < cursorLeft || cursorRight < spanRight) {
+                // 選択範囲が1つのspanに含まれている
+                if (spanLeft < cursorLeft) {
+                    // 左が出てる 左にspanつけなおし
+                    addSpan(RelativeSizeSpan(span.sizeChange), spanLeft, cursorLeft)
+                }
+                if (cursorRight < spanRight) {
+                    // 右が出てる 右にspanつけなおし
+                    addSpan(RelativeSizeSpan(span.sizeChange), cursorRight, spanRight)
+                }
+            }
+        }
+        val largestSize = spans.maxBy { it.sizeChange }!!.sizeChange
+        addSpan(RelativeSizeSpan(largestSize), cursorLeft, cursorRight)
+        edit_text_contents.setSelection(cursorLeft, cursorRight)
     }
 
     override fun onMenuItemClick(item: MenuItem?): Boolean {
@@ -167,7 +338,6 @@ class CreatePostsActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener
     private fun createPosts(isDraft : Boolean) {
         val blogId = intent.getStringExtra(EXTRA_KEY_BLOG_ID)!!
         val title = edit_text_title.text.toString()
-        val content = edit_text_contents.text.toString()
 
         if (!isDraft && title.isEmpty()) {
             // 空 empty title
@@ -179,20 +349,7 @@ class CreatePostsActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener
             return
         }
 
-        // Markdown to HTML
-        val options = MutableDataSet()
-        options.set(
-            Parser.EXTENSIONS,
-            listOf(TablesExtension.create(), StrikethroughExtension.create())
-        )
-        val parser = Parser.builder(options).build()
-        val renderer = HtmlRenderer.builder(options).build()
-        val document = parser.parse(content)
-        val html = renderer.render(document)
-
-        // create Markdown string
-        // val markdown = FlexmarkHtmlConverter.builder(options).build().convert(html)
-
+        val html = Html.toHtml(edit_text_contents.text, Html.TO_HTML_PARAGRAPH_LINES_INDIVIDUAL)
         ApiManager.createPosts(
             blogId,
             title,
