47c6f63a4926e770c2f35f2f67773075ee4a86a9
mkt120 <mame120@gmail.com>
2020-02-09 18:20:15 +0900

    【ラベル入力ダイアログ】 表示時にキーボードを表示した

--- app/src/main/java/com/mkt120/bloggerable/AddLabelDialogFragment.kt
+++ app/src/main/java/com/mkt120/bloggerable/AddLabelDialogFragment.kt
@@ -2,21 +2,26 @@ package com.mkt120.bloggerable
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.dialog_add_label.view.*
 
-
-class AddLabelDialogFragment : DialogFragment() {
+class AddLabelDialogFragment : DialogFragment(), DialogInterface.OnShowListener {
     companion object {
         fun newInstance(): AddLabelDialogFragment =
             AddLabelDialogFragment()
     }
 
+    private var rootView: View? = null
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(requireActivity())
         builder.setTitle(R.string.create_posts_input_label)
-        val rootView =
+        rootView =
             requireActivity().layoutInflater.inflate(R.layout.dialog_add_label, null, false)
         builder.setView(rootView)
         builder.setPositiveButton(android.R.string.ok) { _, _ ->
@@ -29,7 +34,16 @@ class AddLabelDialogFragment : DialogFragment() {
         builder.setNegativeButton(android.R.string.cancel) { _, _ ->
             dismiss()
         }
-        return builder.create()
+        val dialog = builder.create()
+        dialog.setOnShowListener(this)
+        return dialog
+    }
+
+    override fun onShow(p0: DialogInterface?) {
+        rootView!!.edit_text.requestFocus()
+        val inputMethodManager =
+            activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        inputMethodManager!!.showSoftInput(rootView!!.edit_text, 0)
     }
 
     interface OnClickListener {
